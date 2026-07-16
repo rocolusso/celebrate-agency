56a24b64864a78197cd2085f2bc4f120406e9f28
Rocolusso <rocolusso@icloud.com>
2026-07-16 21:51:40 +0300

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
+// import { SpeedInsights } from '@vercel/speed-insights/next';
 import { headers } from 'next/headers';
 import './globals.css';
 
@@ -31,6 +33,8 @@ export default async function RootLayout({
   return (
     <html lang={locale} suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Analytics />
+        {/* <SpeedInsights /> */}
         {children}
       </body>
     </html>
